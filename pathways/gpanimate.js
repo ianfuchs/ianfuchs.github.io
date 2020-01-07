d8156d6b53b882b99d10f1e4352f3b671abf3510
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-07 14:13:13 -0600

    updated JS, maybe

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,16 +1,16 @@
 function toggleDiv(divNum) {
     
     $("#close").hide();
-    $(".slide").animate({right:'-200'},350);
+    $(".slide").animate({left:'-200'},350);
     if($("#div"+divNum)) {
         
-        $("#div"+divNum).animate({right:'0'},350,function(){$("#close").show();});
+        $("#div"+divNum).animate({left:'0'},350,function(){$("#close").show();});
     }
 }
 
 $(document).ready(function(){
    $("#close").on("click",function(e){
-      $(".slide").animate({right:'-200'},350);
+      $(".slide").animate({left:'-200'},350);
       $(this).hide()
    })
 
