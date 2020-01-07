66ddb2e665d41936a559effa2fdbfc4177cda857
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-07 15:05:41 -0600

    refining swap code

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,20 +1,20 @@
-function toggleDiv(divNum) {
-    
-    $("#close").hide();
-    $(".slide").animate({right:'-200'},350);
-    if($("#div"+divNum)) {
-        
-        $("#div"+divNum).animate({right:'0'},350,function(){$("#close").show();});
-    }
-}
-
-$(document).ready(function(){
-   $("#close").on("click",function(e){
-      $(".slide").animate({right:'-200'},350);
-      $(this).hide()
-   })
-
-})
+// function toggleDiv(divNum) {
+//     
+//     $("#close").hide();
+//     $(".slide").animate({right:'-200'},350);
+//     if($("#div"+divNum)) {
+//         
+//         $("#div"+divNum).animate({right:'0'},350,function(){$("#close").show();});
+//     }
+// }
+// 
+// $(document).ready(function(){
+//    $("#close").on("click",function(e){
+//       $(".slide").animate({right:'-200'},350);
+//       $(this).hide()
+//    })
+// 
+// })
 
 //https://stackoverflow.com/questions/43446745/onclick-changing-content-on-div
 
