bcdcb2bd2e6944f6eea3b902dbd6b5d7bf9e67c7
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-07 19:37:46 -0600

    no such thing as an elementbyid

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,12 +1,12 @@
 function swapDiv(divNum) {
   
+var x = document.getElementById("div"+divNum);
 var TextElements = document.getElementsByClassName("slide");
+
 
 for (var i = 0, max = TextElements.length; i < max; i++) {
     TextElements[i].style.display = "none";
 }
-
-  var x = document.getElementById("div"+divNum);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
