0a3647ae75058849d444febe388a3df043ae0f02
Fuchs <fuchs@fuchss-imac.lan>
2020-01-07 19:13:39 -0600

    t trying a 2-pronged approach

---
+++ pathways/gpanimate copy.js
@@ -0,0 +1,32 @@
+function swapDiv(divNum) {
+  
+  var x = document.getElementById("div"+divNum);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+// THIS WAS AN OLD IDEA THAT DIDN'T WORK
+//
+// function toggleDiv(divNum) {
+//     
+//     $("#close").hide();
+//     $(".slide").animate({right:'-200'},350);
+//     if($("#div"+divNum)) {
+//         
+//         $("#div"+divNum).animate({right:'0'},350,function(){$("#close").show();});
+//     }
+// }
+// 
+// $(document).ready(function(){
+//    $("#close").on("click",function(e){
+//       $(".slide").animate({right:'-200'},350);
+//       $(this).hide()
+//    })
+// 
+// })
+
+//https://stackoverflow.com/questions/43446745/onclick-changing-content-on-div
--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,32 +1,14 @@
 function swapDiv(divNum) {
   
-  var x = document.getElementById("div"+divNum);
+var TextElements = document.getElementsById("progDivs");
+
+for (var i = 0, max = TextElements.length; i < max; i++) {
+    TextElements[i].style.display = "none";
+}
+  var x = document.getElementByName("div"+divNum);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 }
-
-
-// THIS WAS AN OLD IDEA THAT DIDN'T WORK
-//
-// function toggleDiv(divNum) {
-//     
-//     $("#close").hide();
-//     $(".slide").animate({right:'-200'},350);
-//     if($("#div"+divNum)) {
-//         
-//         $("#div"+divNum).animate({right:'0'},350,function(){$("#close").show();});
-//     }
-// }
-// 
-// $(document).ready(function(){
-//    $("#close").on("click",function(e){
-//       $(".slide").animate({right:'-200'},350);
-//       $(this).hide()
-//    })
-// 
-// })
-
-//https://stackoverflow.com/questions/43446745/onclick-changing-content-on-div
