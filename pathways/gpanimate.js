e73532856abb44c5afaf844be491c663635ce3bc
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-07 19:20:04 -0600

    swapping name and id

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,11 +1,11 @@
 function swapDiv(divNum) {
   
-var TextElements = document.getElementsById("progDivs");
+var TextElements = document.getElementsByName("progDivs");
 
 for (var i = 0, max = TextElements.length; i < max; i++) {
     TextElements[i].style.display = "none";
 }
-  var x = document.getElementByName("div"+divNum);
+  var x = document.getElementByID("div"+divNum);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
