1a8cad82b041273d5c5878ce953617940fb418ac
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-09 11:07:41 -0600

    new variable

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,13 +1,14 @@
 function swapDiv(divNum) {
   
 var x = document.getElementById("div"+divNum);
+var y = document.getElementById(divNum)
 var TextElements = document.getElementsByClassName("slide");
 
 
 for (var i = 0, max = TextElements.length; i < max; i++) {
     TextElements[i].style.display = "none";
 }
-  if (x === "div8") {
+  if (y == "8") {
     for (var i = 0, max = TextElements.length; i < max; i++) {
     	TextElements[i].style.display = "block";
 	}
