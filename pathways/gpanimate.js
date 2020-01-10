2fc67d786d90f27b5ff9d54c89e6a60954839c4d
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-10 10:41:58 -0600

    added comments for clarity (and for later Ian

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -4,21 +4,24 @@ function swapDiv(divNum) {
     var y = (divNum)
     var TextElements = document.getElementsByClassName("slide");
 
-    console.log(y);
-    console.log(x.style.display);
+   // console.log(y);
+   // console.log(x.style.display);
 
-    if (x.style.display === "block") {
+    if (x.style.display === "block") { 
+    		//find the current status of the selected block. hide if visible
         x.style.display = "none";
     } else {
-
+			// If the current block wasn't visible hide all other blocks
         for (var i = 0, max = TextElements.length; i < max; i++) {
             TextElements[i].style.display = "none";
         }
-        if (y == "8") {
+        if (y == "8") { 
+        	// if the selected block was 8, make all sections appear
             for (var i = 0, max = TextElements.length; i < max; i++) {
                 TextElements[i].style.display = "block";
             }
         } else {
+        	// display the selected block
             x.style.display = "block";
         }
     }
