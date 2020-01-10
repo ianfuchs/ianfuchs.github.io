05be96e6ae365043ac4a7d888b1fbb7642213858
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-10 11:04:47 -0600

    missing curlys

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -4,32 +4,33 @@ function swapDiv(divNum) {
     var y = (divNum)
     var TextElements = document.getElementsByClassName("slide");
 
-   // console.log(y);
-   // console.log(x.style.display);
+    // console.log(y);
+    // console.log(x.style.display);
 
-    if (x.style.display === "block") { 
-    	if (y == "8") { 
-        	// if the selected block was 8, make all sections disappear
+    if (x.style.display === "block") {
+        if (y == "8") {
+            // if the selected block was 8, make all sections disappear
             for (var i = 0, max = TextElements.length; i < max; i++) {
                 TextElements[i].style.display = "none";
+                }
             } else {
-    		//find the current status of the selected block. hide if visible
-        x.style.display = "none"; }
-    } else {
-			// If the current block wasn't visible hide all other blocks
-        for (var i = 0, max = TextElements.length; i < max; i++) {
-            TextElements[i].style.display = "none";
-        }
-        if (y == "8") { 
-        	// if the selected block was 8, make all sections appear
-            for (var i = 0, max = TextElements.length; i < max; i++) {
-                TextElements[i].style.display = "block";
-            }
+                x.style.display = "none"; // hide the single visible block
+        	}
         } else {
-        	// display the selected block
-            x.style.display = "block";
+            // If the current block wasn't visible hide all other blocks
+            for (var i = 0, max = TextElements.length; i < max; i++) {
+                TextElements[i].style.display = "none";
+            }
+            if (y == "8") {
+                // if the selected block was 8, make all sections appear
+                for (var i = 0, max = TextElements.length; i < max; i++) {
+                    TextElements[i].style.display = "block";
+                }
+            } else {
+                // display the selected block
+                x.style.display = "block";
+            }
         }
     }
-}
 }
 
