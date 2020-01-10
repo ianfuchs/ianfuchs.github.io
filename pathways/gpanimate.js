dcd981e86c78b6c909060181a1ca51799d7a4fa0
Ian Fuchs <fuchs.ian@gmail.com>
2020-01-10 10:27:11 -0600

    section on/off toggle

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -1,20 +1,25 @@
 function swapDiv(divNum) {
-  
-var x = document.getElementById("div"+divNum);
-var y = (divNum)
-var TextElements = document.getElementsByClassName("slide");
 
-console.log(y);
+    var x = document.getElementById("div" + divNum);
+    var y = (divNum)
+    var TextElements = document.getElementsByClassName("slide");
 
-for (var i = 0, max = TextElements.length; i < max; i++) {
-    TextElements[i].style.display = "none";
-}
-  if (y == "8") {
-    for (var i = 0, max = TextElements.length; i < max; i++) {
-    	TextElements[i].style.display = "block";
-	}
-  } else {
-    x.style.display = "block";
-  }
+    console.log(y);
+
+    if (x.style.display = "block";) {
+        x.style.display = "none";
+    } else {
+
+        for (var i = 0, max = TextElements.length; i < max; i++) {
+            TextElements[i].style.display = "none";
+        }
+        if (y == "8") {
+            for (var i = 0, max = TextElements.length; i < max; i++) {
+                TextElements[i].style.display = "block";
+            }
+        } else {
+            x.style.display = "block";
+        }
+    }
 }
 
