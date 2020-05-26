b8bec65a6dfa8d60e928498e47ee3be044b2c90b
Ian Fuchs <fuchs.ian@gmail.com>
2020-05-26 12:46:28 -0500

    second set of JS for URL param links

--- pathways/gpanimate.js
+++ pathways/gpanimate.js
@@ -33,3 +33,53 @@ function swapDiv(divNum) {
         }
 }
 
+	// Parse the URL parameter
+	function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('dc');
+ 
+	 $(document).ready(function() {
+ 
+		// Check if the URL parameter is art
+		if (dynamicContent == 'art') {
+			$('#div1').show();
+		} 
+		// Check if the URL parameter is bus
+		else if (dynamicContent == 'bus') {
+			$('#div2').show();
+		} 
+		// Check if the URL parameter is bananas
+		else if (dynamicContent == 'cul') {
+			$('#div3').show();
+		} 
+				// Check if the URL parameter is bananas
+		else if (dynamicContent == 'health') {
+			$('#div4').show();
+		} 
+				// Check if the URL parameter is bananas
+		else if (dynamicContent == 'hum') {
+			$('#div5').show();
+		} 
+				// Check if the URL parameter is bananas
+		else if (dynamicContent == 'tech') {
+			$('#div6').show();
+		} 
+				// Check if the URL parameter is bananas
+		else if (dynamicContent == 'sci') {
+			$('#div7').show();
+		} 
+		// Check if the URL parmeter is empty or not defined, display default content
+		else {
+			$('#default-content').show();
+		}
+	});
+</script>
+
